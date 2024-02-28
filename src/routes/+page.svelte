09cc0676daa01d070a638b6400048011c2992905
Clayton Cook <claytonleonardcook@gmail.com>
2024-02-28 13:27:50 -0500

    cow explode

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,72 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script>
+	import { onMount } from 'svelte';
+
+	/** @type {HTMLImageElement} */
+	let explosionRef;
+	let i = 0;
+
+	let /** @type {HTMLAudioElement} */ mooAudio, /** @type {HTMLAudioElement} */ explodeAudio;
+
+	onMount(() => {
+		mooAudio = new Audio('/moo.mp3');
+		explodeAudio = new Audio('/explode.mp3');
+		explodeAudio.addEventListener('ended', () => {
+			explosionRef?.remove();
+		});
+	});
+
+	/**
+	 * @param {Event} event
+	 */
+	const onClickCow = (event) => {
+		event.preventDefault();
+
+		console.log('Moo');
+		mooAudio.currentTime = 0;
+		mooAudio.play();
+		i++;
+	};
+
+	/**
+	 * @param {Event} event
+	 */
+	const onLoadExplosion = (event) => {
+		event.preventDefault();
+
+		console.log('WTF');
+		explodeAudio.play();
+	};
+</script>
+
+<main>
+	{#if i < 20}
+		<button on:click={onClickCow}>
+			<img src="/cow.png" alt="Black and white calf on transparent background" />
+		</button>
+	{:else}
+		<img bind:this={explosionRef} on:load={onLoadExplosion} src="explosion.gif" alt="Explosion" />
+	{/if}
+</main>
+
+<style>
+	main {
+		display: flex;
+		justify-content: center;
+		align-items: center;
+
+		height: 100dvh;
+		width: 100dvw;
+	}
+
+	button {
+		background: none;
+		border: none;
+
+		cursor: pointer;
+		height: 80%;
+	}
+
+	img {
+		height: 100%;
+	}
+</style>
