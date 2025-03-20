ffb938199282d5197a0364416f94fef9140c8765
Clayton Cook <claytonleonardcook+github@gmail.com>
2025-03-20 17:00:07 -0400

    add conditional to config for preview site url

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,7 +7,7 @@ import svelte from '@astrojs/svelte';
 
 // https://astro.build/config
 export default defineConfig({
-    site: 'https://claycow.com',
+    site: import.meta.env.ENVIRONMENT === 'production' ? 'https://claycow.com' : 'https://preview.claycow.com',
     integrations: [mdx(), sitemap(), svelte()],
     image: {
         domains: ["cdn.bsky.app", "i.imgur.com"],
