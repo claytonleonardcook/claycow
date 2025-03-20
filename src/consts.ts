030a84e5fd459371659216f60cb85acf6842124b
Clayton Cook <claytonleonardcook+github@gmail.com>
2025-03-20 16:39:54 -0400

    add util for grabbing bluesky profile picture

--- src/consts.ts
+++ src/consts.ts
@@ -3,7 +3,6 @@
 
 export const SITE_TITLE = 'claycow';
 export const SITE_DESCRIPTION = 'Welcome to my website!';
-export const PROFILE_IMAGE_LINK = "https://cdn.bsky.app/img/avatar/plain/did:plc:6w63642jircxfpo27gdlihm2/bafkreih2o3ft4llkm2ldxgmuwdjddlsteji76jtogf3ilwib4z5evw6gp4@jpeg";
 
 export const BLUESKY_LINK = "https://bsky.app/profile/did:plc:6w63642jircxfpo27gdlihm2";
 export const GITHUB_LINK = "https://github.com/claytonleonardcook";
